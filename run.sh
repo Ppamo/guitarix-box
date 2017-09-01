0276881aa91723058e8accb01d4e267da3deedd7
Ppamo <pablo@ppamo.cl>
2017-09-01 00:17:39 +0000

    add communication with parent process and fix jackd device name

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ GUITARIX_BAK=guitarix.bak.tgz
 GUITARIX=guitarix
 JACK=jackd
 
+PPID=$(ps -Af | grep $$ | awk -F ' ' '{ print $3 }')
 JACK_PID=$(ps -A | grep $JACK | awk -F' ' '{ print $1 }')
 
 cd $BASEPATH
@@ -25,20 +26,23 @@ kill_process_by_name(){
 }
 
 handle_signal() {
+	kill -s SIGUSR2 $PPID
 	echo ""
 	echo "GB> manejando SIGINT"
 	kill_process_by_name $GUITARIX
 	kill_process_by_name $JACK
+	kill -s SIGUSR1 $PPID
 	exit 0
 }
 
-trap handle_signal SIGINT 
+kill -s SIGUSR2 $PPID
 echo "GB> TRAP SIGINT"
+trap handle_signal SIGINT
 
 if [ -z "$JACK_PID" ]; then
 	echo "GB> JACKD START"
 	export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
-	nohup $JACK -dalsa -r48000 -p1024 -n2 -m -Xseq -D -Chw:U0xd8c0x0c,0 -Phw:U0xd8c0x0c,0 &
+	nohup $JACK -dalsa -r48000 -p1024 -n2 -m -Xseq -D -Chw:Device,0 -Phw:Device,0 &
 	sleep 1
 fi
 JACK_PID=$(ps -A | grep $JACK | awk -F' ' '{ print $1 }')
@@ -56,6 +60,7 @@ if [ -n "$JACK_PID" ]; then
 	echo "GB> NEW GUITAR PID: $GUITARIX_PID"
 fi
 
+kill -s SIGUSR1 $PPID
 while true; do
 	sleep 2
 done
